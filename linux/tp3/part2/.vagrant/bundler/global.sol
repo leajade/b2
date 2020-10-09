ad2af61e7d35676053fca96f797d9fddb5e98080
Lea Duvigneau <lea.duvigneau@ynov.com>
2020-10-09 14:37:58 +0200

    deleted .vagrant folder

--- linux/tp3/part2/.vagrant/bundler/global.sol
+++
@@ -1,1 +0,0 @@
-{"dependencies":[["net-ssh",[">= 2.6.5"]],["net-scp",["~> 1.1"]],["log4r",["~> 1.1"]],["vagrant-scp",["= 0.5.7"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.25.0"]]],"checksum":"cd5d0260c2a216fb550a21c41490f3094e7e3377213eecd833d1ae40d8dc3173","vagrant_version":"2.2.9"}
