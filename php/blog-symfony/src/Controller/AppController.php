a70165b10d2d9c07e2e9b937e4c69d241e975992
Lea Duvigneau <lea.duvigneau@ynov.com>
2020-12-07 10:45:22 +0100

    delete projet/

---
+++ php/blog-symfony/src/Controller/AppController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AppController extends AbstractController
+{
+    /**
+     * @Route("/app", name="app")
+     */
+    public function index()
+    {
+        return $this->render('app/index.html.twig', [
+            'controller_name' => 'AppController',
+        ]);
+    }
+
+    /**
+     * @Route("/about", name="about")
+     */
+    public function about_show(){
+        return $this->render('app/about.html.twig', [
+            'firstName' => 'Léa'
+        ]);
+    }
+}
